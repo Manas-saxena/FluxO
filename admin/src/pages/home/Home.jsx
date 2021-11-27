1947ba3f0d627274a2f5b9f4fa5809bdf95f4ca8
Manas Saxena <manassaxena160601@gmail.com>
2021-11-28 01:52:08 +0530

    charts added to home page

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./home.css"
+import Chart from "../../component/charts/Chart"
 import Featuredinfo from '../../component/featuredinfo/Featuredinfo'
 const Home = () => {
     return (
         <div className='home'>
-            home
 <Featuredinfo></Featuredinfo>
+        <Chart></Chart>
         </div>
     )
 }
